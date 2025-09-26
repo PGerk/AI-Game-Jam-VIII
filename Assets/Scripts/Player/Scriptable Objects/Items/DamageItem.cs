4157dc42f19c0e7cd03c58298778c0e3ab1f60d5
PGerk <fsau@gmx.de>
2025-09-26 10:31:20 +0200

    Consolidated Magic and Items

--- Assets/Scripts/Player/Scriptable Objects/Items/DamageItem.cs
+++ Assets/Scripts/Player/Scriptable Objects/Items/DamageItem.cs
@@ -6,11 +6,25 @@ public class DamageItem : SOItem
     public int damage;
     public override void Use(PlayerCharacter player, Target target)
     {
-        if(isReady)
+        if(!isMagic && isReady)
         {
             StartCooldown();
             target.takeDamage(damage);
+            return;
         }
+        if(isMagic)
+        {
+            if(DeductMP(player))
+            {
+                target.takeDamage(damage);
+            }
+            else
+            {
+                Debug.Log("Not enough MP! Remaining: " + player.stats.mp + " of needed " + mpCost);
+            }
+            return;
+        }
+        Debug.Log("Item not ready! Cooldown remaining: " + cooldownTimer);
     }
 
     public override void OnAdvanceTurn()
--- Assets/Scripts/Player/Scriptable Objects/Items/SOItem.cs
+++ Assets/Scripts/Player/Scriptable Objects/Items/SOItem.cs
@@ -9,40 +9,70 @@ public abstract class SOItem : ScriptableObject
     [TextArea] public string description;
     public Sprite icon;
 
+    //Regular Items
     public int cooldown;
     protected int cooldownTimer;
     public bool isReady;
 
-    /// <summary>
-    /// Benutzt das Item.
-    /// </summary>
-    /// <param name="player">Referenz auf den Spielercharakter.</param>
-    /// <param name="target">Das Ziel (im Regelfall Gegner).</param>
+    //Magic Items
+    public bool isMagic;
+    public int mpCost = 1;
+
+    ///<summary>
+    ///Benutzt das Item.
+    ///</summary>
+    ///<param name="player">Referenz auf den Spielercharakter.</param>
+    ///<param name="target">Das Ziel (im Regelfall Gegner).</param>
     public abstract void Use(PlayerCharacter player, Target target);
-    /// <summary>
-    /// Sachen, die zwischen den Zügen passieren (vor allem cooldown).
-    /// </summary>
+    ///<summary>
+    ///Sachen, die zwischen den Zügen passieren (vor allem cooldown).
+    ///</summary>
     public abstract void OnAdvanceTurn();
-    /// <summary>
-    /// Startet cooldown.
-    /// </summary>
+    ///<summary>
+    ///Startet cooldown.
+    ///</summary>
     protected void StartCooldown()
     {
         cooldownTimer = cooldown;
         isReady = false;
     }
-    /// <summary>
-    /// Sollte in AdvanceTurn, führt Cooldown fort.
-    /// </summary>
+
+    ///<summary>
+    ///Sollte in AdvanceTurn, führt Cooldown fort.
+    ///</summary>
     protected void AdvanceCooldown()
     {
         if (!isReady && cooldownTimer > 0)
         {
             cooldownTimer--;
+            Debug.Log("Cooldown timer down to " + cooldownTimer);
+            return;
         }
         if (!isReady && cooldownTimer < 1)
         {
             isReady = true;
+            Debug.Log("Item read!");
+            return;
+        }
+        Debug.Log("No cooldown timer to decrease.");
+        return;
+    }
+
+    ///<summary>
+    ///Bezahlt die MP-Kosten, gibt TRUE bei Erfolg, FALSE bei Misserfolg zurück.
+    ///</summary>
+    ///<param name="player">Referenz auf den Spielercharakter.</param>
+    protected bool DeductMP(PlayerCharacter player)
+    {
+        if (player.stats.mp >= mpCost)
+        {
+            player.stats.mp -= mpCost;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
+}
 }
